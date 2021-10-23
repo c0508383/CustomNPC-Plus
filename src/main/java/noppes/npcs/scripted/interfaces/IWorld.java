9267212b75ad9bf1de4002022ed1aaf94799b7ee
Enderverse <storiomario@gmail.com>
2021-10-23 22:01:47 +0200

    public is redundant for interface methods

--- src/main/java/noppes/npcs/scripted/interfaces/IWorld.java
+++ src/main/java/noppes/npcs/scripted/interfaces/IWorld.java
@@ -12,12 +12,12 @@ public interface IWorld {
     /**
      * @return The worlds time
      */
-    public long getTime();
+    long getTime();
 
     /**
      * @return The total world time
      */
-    public long getTotalTime();
+    long getTotalTime();
 
     /**
      * @param x World position x
@@ -25,7 +25,7 @@ public interface IWorld {
      * @param z World position z
      * @return The block at the given position. Returns null if there isn't a block
      */
-    public IBlock getBlock(int x, int y, int z);
+    IBlock getBlock(int x, int y, int z);
 
     /**
      * @param x World position x
@@ -34,7 +34,7 @@ public interface IWorld {
      * @return Text from signs
      * @since 1.7.10d
      */
-    public String getSignText(int x, int y, int z);
+    String getSignText(int x, int y, int z);
 
     /**
      * @param x World position x
@@ -42,47 +42,47 @@ public interface IWorld {
      * @param z World position z
      * @param item The block to be set
      */
-    public void setBlock(int x, int y, int z, ScriptItemStack item);
+    void setBlock(int x, int y, int z, ScriptItemStack item);
 
     /**
      * @param x World position x
      * @param y World position y
      * @param z World position z
      */
-    public void removeBlock(int x, int y, int z);
+    void removeBlock(int x, int y, int z);
 
     /**
      * @param name The name of the player to be returned
      * @return The Player with name. Null is returned when the player isnt found
      */
-    public ScriptPlayer getPlayer(String name);
+    ScriptPlayer getPlayer(String name);
 
     /**
      * @param time The world time to be set
      */
-    public void setTime(long time);
+    void setTime(long time);
 
     /**
      * @return Whether or not its daytime
      */
-    public boolean isDay();
+    boolean isDay();
 
     /**
      * @return Whether or not its currently raining
      */
-    public boolean isRaining();
+    boolean isRaining();
 
     /**
      * @param bo Set if it's raining
      */
-    public void setRaining(boolean bo);
+    void setRaining(boolean bo);
 
     /**
      * @param x The x position
      * @param y The y position
      * @param z The z position
      */
-    public void thunderStrike(double x, double y, double z);
+    void thunderStrike(double x, double y, double z);
 
     /**
      * Sends a packet from the server to the client everytime its called. Probably should not use this too much.
@@ -96,7 +96,7 @@ public interface IWorld {
      * @param speed Speed of the particles, usually between 0 and 1
      * @param count Particle count
      */
-    public void spawnParticle(String particle, double x, double y, double z, double dx, double dy, double dz, double speed, int count);
+    void spawnParticle(String particle, double x, double y, double z, double dx, double dy, double dz, double speed, int count);
 
     /**
      * @param id The items name
@@ -104,71 +104,71 @@ public interface IWorld {
      * @param size The number of items in the item
      * @return Returns the item
      */
-    public ScriptItemStack createItem(String id, int damage, int size);
+    ScriptItemStack createItem(String id, int damage, int size);
 
     /**
      * @param directory The particle's texture directory. Use only forward slashes when writing a directory. Example: "customnpcs:textures/particle/tail.png"
      * @return Returns ScriptEntityParticle object
      */
-    public ScriptEntityParticle createEntityParticle(String directory);
+    ScriptEntityParticle createEntityParticle(String directory);
 
     /**
      * @param key Get temp data for this key
      * @return Returns the stored temp data
      */
-    public Object getTempData(String key);
+    Object getTempData(String key);
 
     /**
      * Tempdata gets cleared when the server restarts. All worlds share the same temp data.
      * @param key The key for the data stored
      * @param value The data stored
      */
-    public void setTempData(String key, Object value);
+    void setTempData(String key, Object value);
 
     /**
      * @param key The key thats going to be tested against the temp data
      * @return Whether or not temp data containes the key
      */
-    public boolean hasTempData(String key);
+    boolean hasTempData(String key);
 
     /**
      * @param key The key for the temp data to be removed
      */
-    public void removeTempData(String key);
+    void removeTempData(String key);
 
     /**
      * Removes all tempdata
      */
-    public void clearTempData();
+    void clearTempData();
 
     /**
      * @param key The key of the data to be returned
      * @return Returns the stored data
      */
-    public Object getStoredData(String key);
+    Object getStoredData(String key);
 
     /**
      * Stored data persists through world restart. Unlike tempdata only Strings and Numbers can be saved
      * @param key The key for the data stored
      * @param value The data stored. This data can be either a Number or a String. Other data is not stored
      */
-    public void setStoredData(String key, Object value);
+    void setStoredData(String key, Object value);
 
     /**
      * @param key The key of the data to be checked
      * @return Returns whether or not the stored data contains the key
      */
-    public boolean hasStoredData(String key);
+    boolean hasStoredData(String key);
 
     /**
      * @param key The key of the data to be removed
      */
-    public void removeStoredData(String key);
+    void removeStoredData(String key);
 
     /**
      * Remove all stored data
      */
-    public void clearStoredData();
+    void clearStoredData();
 
     /**
      * @param x Position x
@@ -178,9 +178,9 @@ public interface IWorld {
      * @param fire Whether or not the explosion does fire damage
      * @param grief Whether or not the explosion does damage to blocks
      */
-    public void explode(double x, double y, double z, float range, boolean fire, boolean grief);
+    void explode(double x, double y, double z, float range, boolean fire, boolean grief);
 
-    public ScriptPlayer[] getAllServerPlayers();
+    ScriptPlayer[] getAllServerPlayers();
 
     /**
      * @since 1.7.10c
@@ -188,7 +188,7 @@ public interface IWorld {
      * @param z Position z
      * @return Returns the name of the biome
      */
-    public String getBiomeName(int x, int z);
+    String getBiomeName(int x, int z);
 
     /**
      * Lets you spawn a server side cloned entity
@@ -199,9 +199,9 @@ public interface IWorld {
      * @param name Name of the cloned entity
      * @return Returns the entity which was spawned
      */
-    public ScriptEntity spawnClone(int x, int y, int z, int tab, String name);
+    ScriptEntity spawnClone(int x, int y, int z, int tab, String name);
 
-    public ScriptScoreboard getScoreboard();
+    ScriptScoreboard getScoreboard();
 
     BlockPos getMCBlockPos(int x, int y, int z);
 
@@ -210,5 +210,5 @@ public interface IWorld {
      * Expert use only
      * @return Returns minecraft world object
      */
-    public WorldServer getMCWorld();
+    WorldServer getMCWorld();
 }
